815d4d4dfeda1bd1df6b021a1febde7a53b6c9d2
Dezmond-sama <Dezmond-sama@mail.ru>
2024-02-01 14:15:25 +0300

    Day 2. Added solution to task 3.

---
+++ Lesson_2/Task_3/main.go
@@ -0,0 +1,69 @@
+// # Шахматная доска
+
+// ## Вход:
+
+// размер шахматной доски, от 0 до 20
+
+// ## Выход:
+
+// вывести на экран эту доску, заполняя поля нулями и единицами
+
+// ## Пример:
+
+// Вход: 5
+// Выход:
+// 0 1 0 1 0
+// 1 0 1 0 1
+// 0 1 0 1 0
+// 1 0 1 0 1
+// 0 1 0 1 0
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func PrintRow(len int, first, second rune) {
+	for i := 0; i < len; i++ {
+		var cell rune = first
+		// Use the second rune if it is an odd cell
+		if i%2 == 1 {
+			cell = second
+		}
+		fmt.Printf("%c ", cell)
+	}
+	fmt.Println()
+}
+
+func PrintBoard(size int, first, second rune) {
+	for i := 0; i < size; i++ {
+		// Change the order of the first and second runes depending on the evenness of the row
+		if i%2 == 0 {
+			PrintRow(size, first, second)
+			continue
+		}
+		PrintRow(size, second, first)
+	}
+}
+
+func main() {
+	var size int
+
+	// Input
+	fmt.Print("Enter the size of the board: ")
+	_, err := fmt.Scanf("%d\n", &size)
+
+	// Checks
+	if err != nil {
+		log.Fatal("Wrong input: ", err)
+	}
+	if size < 0 || size > 20 {
+		log.Fatalf("Size should be in range [0, 20], got %d", size)
+	}
+
+	// Output
+	fmt.Println("Your board:")
+	PrintBoard(size, '0', '1')
+}
