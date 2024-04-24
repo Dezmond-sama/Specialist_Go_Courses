64d63c89c63f0a51fda4e0ba3614df9eec50e283
Dezmond-sama <dezmond-sama@mail.ru>
2024-04-24 13:01:21 +0300

    GO3. Bankstore. Added CRUD requests and generated code by sqlc.

---
+++ GO3/Day2/bankstore/db/schema/schema.sql
@@ -0,0 +1,34 @@
+CREATE TYPE Currency AS ENUM ('USD', 'EUR', 'RUB');
+
+CREATE TABLE IF NOT EXISTS accounts (
+    id BIGSERIAL PRIMARY KEY,
+    owner VARCHAR(255) NOT NULL,
+    balance DECIMAL(10, 2) NOT NULL,
+    currency Currency NOT NULL,
+    created TIMESTAMP NOT NULL default now()
+);
+
+CREATE TABLE entries (
+    id BIGSERIAL PRIMARY KEY,
+    account_id BIGINT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+    amount DECIMAL(10, 2) NOT NULL,
+    created TIMESTAMP NOT NULL default now()
+);
+
+CREATE TABLE transfers (
+    id BIGSERIAL PRIMARY KEY,
+    from_account_id BIGINT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+    to_account_id BIGINT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+    amount DECIMAL(10, 2) NOT NULL,
+    created TIMESTAMP NOT NULL default now()
+);
+
+CREATE INDEX ON accounts (owner);
+
+CREATE INDEX ON entries (account_id);
+
+CREATE INDEX ON transfers (from_account_id);
+
+CREATE INDEX ON transfers (to_account_id);
+
+CREATE INDEX ON transfers (from_account_id, to_account_id);
